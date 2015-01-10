7af99b6ee9a6bf8d730885d87894ed8a2d10448a
Erik van den Berg <erik@creativeskills.nl>
2015-01-10 08:48:57 +0100

    Fail...

--- www/imageresizeConstants.js
+++ www/imageresizeConstants.js
@@ -23,12 +23,12 @@ cordova.define("nl.creativeskills.cordova.imageresize.ImageResize", function(req
 module.exports = {
     ImageResizer: {
         IMAGE_DATA_TYPE_BASE64: "base64Image",
-        IMAGE_DATA_TYPE_URL = "urlImage",
-        RESIZE_TYPE_FACTOR = "factorResize",
-        RESIZE_TYPE_PIXEL = "pixelResize",
-        FORMAT_JPG = "jpg",
-        FORMAT_PNG = "png",
-        FORMAT_PDF = "pdf"
+        IMAGE_DATA_TYPE_URL: "urlImage",
+        RESIZE_TYPE_FACTOR: "factorResize",
+        RESIZE_TYPE_PIXEL: "pixelResize",
+        FORMAT_JPG: "jpg",
+        FORMAT_PNG: "png",
+        FORMAT_PDF: "pdf"
     }
 };
 
